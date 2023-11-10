6ff27ac37fba2c104a269b2b323da0c38dd272bf
HC mbp21 <harrchiu@gmail.com>
2023-11-10 01:18:51 +0100

    we glowing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
-const KBKey: React.FC<{ text: string; isPressed: boolean }> = ({ text, isPressed }) => {
-  //  style={{ marginTop: isPressed ? '20px' : 0 }}
+const DEFAULT_KEYPRESS_MS = 200;
+
+const KBKey: React.FC<{ text: string; isPressed: boolean; speed: number }> = ({
+  text,
+  isPressed,
+  speed,
+}) => {
   return (
-    <div className={`keyboard-key ${isPressed && 'roll-out'}`}>
+    <div
+      className={`keyboard-key ${isPressed && 'roll-out'}`}
+      style={{
+        animationDuration: isPressed ? `${DEFAULT_KEYPRESS_MS / speed / 1000}s` : undefined,
+      }}
+    >
       <div className='keyboard-bottom-circle' />
       <div className='keyboard-rect' />
       <div className='keyboard-top-circle'>{text}</div>
@@ -17,37 +28,104 @@ const keyboardKeys = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'"],
   ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.'],
+  [' '],
 ];
-
-const rowOffsets = [0, 0, 100, 40];
+const rowOffsets = [0, 0, 100, 40, 0, 50];
 
 // programatically make dictionary of key positions
-let keyPositions: { [key: string]: { row: number; col: number } } = {};
-keyboardKeys.forEach((row, rowId) => {
-  row.forEach((key, colId) => {
-    keyPositions[key] = { row: rowId, col: colId };
-  });
-});
+// let keyPositions: { [key: string]: { row: number; col: number } } = {};
+// keyboardKeys.forEach((row, rowId) => {
+//   row.forEach((key, colId) => {
+//     keyPositions[key] = { row: rowId, col: colId };
+//   });
+// });
+
+interface IContent {
+  text: string;
+  speed?: number;
+  postDelay?: number; // ms
+
+  // content
+  url?: string;
+
+  // styling
+  bold?: boolean;
+  italic?: boolean;
+  glow?: boolean;
+  underline?: boolean;
+}
+
+const getTextElement = (content: IContent, charIndex?: number) => {
+  const style = {
+    fontWeight: content.bold ? 'bold' : undefined,
+    fontStyle: content.italic ? 'italic' : undefined,
+    textDecoration: content.underline ? 'underline' : undefined,
+  };
+
+  const renderedText = content.text.slice(0, charIndex);
+  const className = content.glow ? 'glowing-text' : undefined;
+
+  if (content.url) {
+    return (
+      <a
+        href={content.url}
+        style={style}
+        target='_blank'
+        rel='noopener noreferrer'
+        className={className}
+      >
+        {renderedText}
+      </a>
+    );
+  }
+  return (
+    <span style={style} className={className}>
+      {renderedText}
+    </span>
+  );
+};
 
 function App() {
-  const fileString = ` Shall I compare thee to a summer's day?   
-  Thou art more lovely and more temperate:
-  Rough winds do shake the darling buds of May,
-  And summer's lease hath all too short a date:
-  Sometime too hot the eye of heaven shines,
-  And often is his gold complexion dimm'd;
-  And every fair from fair sometime declines,
-  * The 154 sonnets Shakespeare wrote exemplify his talent for compressed writing and depth of thought. Generally thought to be (at least to some degree) autobiographical, many are in the nature of apostrophic generalities; a large number are addressed to a man, others to a "dark lady." The identity of these persons addressed has generated considerable conjecture and a torrent of controversy .
-  By chance or nature's changing course untrimm'd;
-  But thy eternal summer shall not fade,
-  Nor lose possession of that fair thou owest,
-  Nor shall Death brag thou wander'st in his shade,
-  When in eternal lines to time thou grow'st;
-      So long as men can breathe, or eyes can see,
-      So long lives this, and this gives life to thee.
-  `;
+  const fileContent: IContent[] = [
+    // { text: `Hello.\n\n`, speed: 2, postDelay: 400 },
+    // { text: `I am a `, speed: 3, postDelay: 1000 },
+    // { text: `typewriter`, speed: 1.3, postDelay: 200, bold: true, italic: true, glow: true },
+    // { text: `...`, speed: 0.5, postDelay: 1250 },
+
+    { text: `Hello.\n\n`, speed: 50 },
+    { text: `I am a `, speed: 50 },
+    { text: `typewriter`, speed: 50, glow: true },
+
+    { text: ` made by Harrison. \n`, postDelay: 200, speed: 10 },
+    { text: `That is all he wanted to do, thanks.\n\n`, speed: 20 },
+    { text: `Here is: `, speed: 10 },
+    { text: `\n - his `, speed: 10 },
+    { text: `resume`, speed: 10, url: `https://harrchiu.me/Harrison_Chiu_Resume.pdf` },
+    { text: `\n - his `, speed: 10 },
+    { text: `linkedin`, speed: 10, url: `https://www.linkedin.com/in/harrchiu/` },
+    { text: `\n - his `, speed: 10 },
+    { text: `github`, speed: 10, url: `https://github.com/harrchiu` },
+    { text: `\n - his email (harrchiu@gmail.com)`, speed: 10 },
+    { text: `\n - my `, speed: 10, italic: true },
+    { text: `github??`, speed: 10, url: `https://github.com/harrchiu/portfolio` },
+  ];
+
   const [pressedKeys, setPressedKeys] = useState<Set<string>>(new Set());
-  const [typedText, setTypedText] = useState('');
+  const [typedState, setTypedState] = useState({
+    contentIndex: 0,
+    charIndex: 0,
+  });
+
+  const getRenderedContent = () => {
+    const { contentIndex, charIndex } = typedState;
+
+    let renderedContent: React.ReactNode[] = [];
+    for (let i = 0; i < contentIndex; i++) {
+      renderedContent.push(getTextElement(fileContent[i]));
+    }
+    renderedContent.push(getTextElement(fileContent[contentIndex], charIndex));
+    return renderedContent;
+  };
 
   const handleKeyDown = (e: KeyboardEvent) => {
     const newPressedKeys = new Set(pressedKeys);
@@ -72,21 +150,30 @@ function App() {
   // time out to gradually show the filestring as if it's being typed
   useEffect(() => {
     let timeout = 0;
-    for (let i = 0; i < fileString.length; i++) {
-      timeout += 500; // + (Math.random() > 0.5 ? 50 : 0);
-      console.log(timeout, i, fileString.length);
-      setTimeout(() => {
-        const newPressedKeys = new Set(pressedKeys);
-        newPressedKeys.add(fileString[i].toUpperCase());
-        setPressedKeys(newPressedKeys);
-        setTypedText(fileString.slice(0, i + 1));
-      }, timeout);
+    for (let i = 0; i < fileContent.length; i++) {
+      for (const char of fileContent[i].text) {
+        timeout += DEFAULT_KEYPRESS_MS / (fileContent[i]?.speed ?? 1);
+        setTimeout(() => {
+          const newPressedKeys = new Set(pressedKeys);
+          newPressedKeys.add(char.toUpperCase());
+          setPressedKeys(newPressedKeys);
+          setTypedState((prev) => ({ ...prev, charIndex: prev.charIndex + 1 }));
+        }, timeout);
+      }
+      timeout += fileContent[i]?.postDelay ?? 0;
+
+      // set timeout for next content
+      if (i !== fileContent.length - 1) {
+        setTimeout(() => {
+          setTypedState((prev) => ({ ...prev, contentIndex: prev.contentIndex + 1, charIndex: 0 }));
+        }, timeout);
+      }
     }
   }, []);
 
   return (
     <div className='App'>
-      <div className='typed-paper'>{typedText}</div>
+      <div className='typed-paper'>{getRenderedContent()}</div>
       <div className='keyboard-area'>
         {keyboardKeys.map((row, rowId) => {
           return (
@@ -98,7 +185,12 @@ function App() {
               {row.map((key, colId) => {
                 const text = keyboardKeys[rowId][colId];
                 return (
-                  <KBKey text={text} isPressed={pressedKeys.has(text)} key={`kb-key-${text}`} />
+                  <KBKey
+                    text={text}
+                    isPressed={pressedKeys.has(text)}
+                    key={`kb-key-${text}`}
+                    speed={1}
+                  />
                 );
               })}
             </div>
